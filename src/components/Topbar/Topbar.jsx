24654c0b75e12f1611593a569155ab7c3e7d9646
danut nanu <125583127+danutnanu@users.noreply.github.com>
2025-07-22 20:23:09 +0100

    Add Bootstrap class for border

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -4,7 +4,7 @@ import './Topbar.css'
 const Topbar = () => {
   return (
     <>
-      <div className="topbar fixed-top text-center p-3 me-2 border border-1 rounded-bottom">
+      <div className="topbar fixed-top text-center p-3 me-2 border border-1 border-dark rounded-bottom">
         <div className="title-container rounded-3 bg-light border border-1 border-dark">
           <img src="/icon.ico" alt="RemoteTrends Logo" className='mt-3 ms-2' style={{ width: '50px', height: '50px' }} />
           <div className="title-swimming rounded-4 py-2 px-3 m-1 border border-1 border-dark">
